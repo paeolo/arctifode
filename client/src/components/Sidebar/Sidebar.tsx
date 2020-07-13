c7bf086a8a112ed99aef4fd751e0d2d2b15df070
Paul Le Couteur <paullecouteur@gmail.com>
2020-07-13 15:27:06 +0200

    better off-canvas layout (overflow-y for sidebar)

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -13,7 +13,10 @@ export const Sidebar: React.FC = () => {
       <Item label='Artifacts' icon='fas fa-industry' />
       <Item label='Releases' icon='fas fa-truck' />
       <Item label='Environments' icon='fas fa-server' />
-      <Item label='Deployments' icon='fas fa-paper-plane' />
+      <Item label='Deployments' icon='fas fa-rocket' />
+      <Item label='Tasks' icon='fas fa-tasks' />
+      <Item label='Members' icon='fas fa-users' />
+      <Item label='Settings' icon='fas fa-cog' />
     </React.Fragment>
   );
 }
